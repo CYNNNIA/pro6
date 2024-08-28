c24d318e990f02c40207b5ae919335abc9b59432
cynnnia <cynthialorenzolopez@gmail.com>
2024-08-28 18:55:53 +0200

    .

--- tests/peliculas.test.js
+++ tests/peliculas.test.js
@@ -1,11 +1,10 @@
-const request = require('supertest') // Importa Supertest para realizar solicitudes HTTP
-const app = require('../index') // Importa la aplicación Express
-
+const request = require('supertest')
+const app = require('../index')
 describe('GET /api/v1/peliculas', () => {
   it('debería devolver todas las películas', async () => {
     const res = await request(app).get('/api/v1/peliculas')
-    expect(res.statusCode).toEqual(200) // Verifica que el código de estado sea 200 (OK)
-    expect(res.body).toBeInstanceOf(Array) // Verifica que la respuesta sea un array
+    expect(res.statusCode).toEqual(200)
+    expect(res.body).toBeInstanceOf(Array)
   })
 })
 
@@ -16,11 +15,11 @@ describe('POST /api/v1/peliculas', () => {
       .send({
         nombre: 'New Movie',
         categoria: 'aventura',
-        plataformas: ['<ID de Plataforma>'], // Sustituye con un ID válido de una plataforma
+        plataformas: ['<ID de Plataforma>'],
         precio: 9.99
       })
-      .set('Authorization', 'Bearer <your_token>') // Añade el token de autenticación
-    expect(res.statusCode).toEqual(201) // Verifica que el código de estado sea 201 (Created)
-    expect(res.body).toHaveProperty('nombre', 'New Movie') // Verifica que la película creada tiene el nombre correcto
+      .set('Authorization', 'Bearer <your_token>')
+    expect(res.statusCode).toEqual(201)
+    expect(res.body).toHaveProperty('nombre', 'New Movie')
   })
 })
